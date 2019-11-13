8e6c3be85f98940fb33dc23327b3e4e88f4250b6
cjogles <jackson.ogles28@gmail.com>
2019-11-13 13:03:48 -0700

    Gallery Data and Gallery comp set up structure

---
+++ src/data/tileData.js
@@ -0,0 +1,33 @@
+export const tileData = [
+    {
+        img: 'public\images\resume.jpg',
+        title: 'Project',
+        author: 'author',
+    },
+    {
+        img: 'public\images\work-station.jpg',
+        title: 'Image',
+        author: 'author',
+    },
+    {
+        img: 'public\images\resume.jpg',
+        title: 'Image',
+        author: 'author',
+    },
+    {
+        img: 'public\images\resume.jpg',
+        title: 'Image',
+        author: 'author',
+    },
+    {
+        img: 'public\images\work-station.jpg',
+        title: 'Image',
+        author: 'author',
+    },
+    {
+        img: 'public\images\resume.jpg',
+        title: 'Image',
+        author: 'author',
+    },
+
+]

cc192ce32e95f9b4dd79c82517491065c5125b7c
cjogles <jackson.ogles28@gmail.com>
2019-11-13 14:07:33 -0700

    image test

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
                     target="_blank"
                 >
                     <img 
-                        src="./ikigai.jpg" 
+                        src="/ikigai.jpg" 
                         alt="ikigai = a japanese word/notion for life meaning being found" 
                     />
                 </a>

448fb57c6ee8387674c788cab5b39d2e432b86cf
cjogles <jackson.ogles28@gmail.com>
2019-11-13 12:35:15 -0700

    Changed home back to no navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,9 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Slide from '@material-ui/core/Slide';
-
-function HideOnScroll(props) {
-    const { children, window } = props;
-    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+const Home = () => {
     return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
+        <h2>Home</h2>
+    )
 }
 
-HideOnScroll.propTypes = {
-    children: PropTypes.element.isRequired,
-    window: PropTypes.func,
-  };
-
-const Home = (props) => {
-<React.Fragment>
-    <CssBaseline />
-    <HideOnScroll {...props}>
-        <AppBar>
-            <Toolbar>
-                <Typography variant="h6">Scroll to Hide App Bar</Typography>
-            </Toolbar>
-        </AppBar>
-    </HideOnScroll>
-    <Toolbar />
-    <Container>
-        <Box my={2}>
-            {[...new Array(12)].map(() => 
-            `Cras mattis consectetur purus sit amet fermentum.
-            Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-            Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>
-    </Container>
-</React.Fragment>
-}
 export default Home;

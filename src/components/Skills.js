198b9bc6f8d26dbaaf5295b2290b73db6b2f172c
cjogles <jackson.ogles28@gmail.com>
2019-11-13 13:30:29 -0700

    Skills Structure built

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,78 @@
 import React from 'react';
 
-const Skills = () => {
-    return (
-        <h2>Skills</h2>
-    )
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
+
+function createData(name, assets) {
+  return { name, assets };
 }
 
-export default Skills;
+const rows = [
+  createData('HTML/CSS', 'Preprocessing w/ Less, Styling w/ Flexbox, Mobile Responsive Design, SEO mindful'),
+  createData('JavaScript', 'OOP, Closures, Higher Order Functions, Variable Scope & Hoisting, TDD,'),
+  createData('React', 'Function Components w/ Hooks, Class Components w/ Life Cycle Methods, Higher Order Components, Context'),
+  createData('Redux', 'Action, Action Creators, Reducers, Store, Async API Calls with Axios'),
+  createData('FrontEnd Style', 'Material UI --> Used it to build this portfolio in addition to vanilla CSS'),
+  createData('NodeJS', 'To Be Continued...'),
+];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+}));
+
+export default function Skills() {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Skills (by topic)</StyledTableCell>
+            <StyledTableCell align="right">Assets these Skills Bring to the Table</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.assets}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}

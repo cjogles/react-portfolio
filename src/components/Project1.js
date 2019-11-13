7d9fd017d7c7003648063ef29c03e22cf9df8e66
cjogles <jackson.ogles28@gmail.com>
2019-11-13 12:49:09 -0700

    project 1 comp structure completed

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Project1 = () => {
     return (
-        <h2>Project1</h2>
+        <>
+            <img src="public\images\binary.jpg" alt="picture of notebook with binary code on it"/>
+            <div>
+                <h2>Github API Traversal</h2>
+                <p>
+                    Traversed Github API for user data using axios library. 
+                    Created social cards via vanilla javascript instead of 
+                    using the React Library.
+                </p>
+                <div>
+                    <button>Explore Project</button>
+                    <div>
+                        <a href="https://cjogles.github.io/github-usercard/" target="_blank">View Website</a>
+                        <a href="https://github.com/cjogles/github-usercard" target="_blank">View Code</a>
+                    </div>
+                </div> 
+            </div>
+        </>
     )
 }
 

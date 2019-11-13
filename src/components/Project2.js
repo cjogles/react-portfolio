432277931fa63aa0c5ce73c84e6fd18d102fc76f
cjogles <jackson.ogles28@gmail.com>
2019-11-13 12:53:31 -0700

    Project 2 structure completed

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Project2 = () => {
     return (
-        <h2>Project2</h2>
+        <>
+            <div>
+                <h2>Project Title Second Best Insert Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Itaque reprehenderit natus corporis blanditiis vero ducimus? 
+                    Laborum, omnis. Harum ullam optio distinctio quasi excepturi cum nobis! 
+                    Culpa, commodi. Aut, tenetur eveniet.
+                </p>
+                <div>
+                    <button>Explore Project</button>
+                    <div>
+                        <a href="#">View Website</a>
+                        <a href="#">View Code</a>
+                    </div>
+                </div> 
+            </div>
+            <img src="public\images\circuit-board.jpg" alt="picture of circuit board floating in the air"/>
+        </>
     )
 }
 

52640e95639bebddc5addbb10c50f2deea26d769
cjogles <jackson.ogles28@gmail.com>
2019-11-13 12:43:17 -0700

    basic home comp structure built

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <h2>Home</h2>
+        <>
+            <div>
+                <h1>Jackson Ogles</h1>
+                <p> 
+                    My ikigai (pronounced "aye-kee-guy") is centered 
+                    in software engineering principles and using them in a practical way.
+                    You could say it's the reason I get up in the morning!
+                </p>
+                <p>🛌 ➡ 🕺</p>
+                <p><a>View Projects</a></p>
+            </div>
+            <div>
+                <a 
+                    href="https://en.wikipedia.org/wiki/Ikigai" 
+                    target="_blank"
+                >
+                    <img 
+                        src="public\images\ikigai.jpg" 
+                        alt="ikigai = a japanese word/notion for life meaning being found" 
+                    />
+                </a>
+            </div>
+        </>
     )
 }
 

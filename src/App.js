1aff8547631544d0138cc1ef3f205eed71a2ecb4
cjogles <jackson.ogles28@gmail.com>
2019-11-13 12:00:39 -0700

    container set up in index.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,15 @@ import Project2 from './components/Project2';
 import Gallery from './components/Gallery';
 import Form from './components/Form';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
 function App() {
   
   return (
     <div className="App">
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Typography component={
-            <Home />,
-            <Project1 />,
-            <Project2 />,
-            <Gallery />,
-            <Form />
-            } 
-            style={{ backgroundColor: '#cfe8fc', height: '100vh' }} 
-            />
-        </Container>
-      </React.Fragment>
+      <Home />
+      <Project1 />
+      <Project2 />
+      <Gallery />
+      <Form />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography component={<App/>} style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

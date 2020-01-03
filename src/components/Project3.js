f78e81e7d055075b72196fb98eb02e594438037e
cjogles <jackson.ogles28@gmail.com>
2020-01-03 13:58:13 -0700

    added plant purpose

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -8,35 +8,39 @@ const Project3 = () => {
         <div className="firstProject1">
           <a href="https://project-list.now.sh" target="_blank">
             <img 
-            src="images/todo.jpg" 
-            alt="screenshot of block-club calendar website home page" 
+            src="images/plantScreenShot.jpg" 
+            alt="screenshot of plant purpose website home page" 
             />
           </a>
           <div className="proj1Text">
-            <h2>ToDo List</h2>
+            <h2>Plant Purpose</h2>
             <p>
-              Created a ToDo list App front end client via react and back end 
-              server and database via sqlite data base management system and 
-              knex and express. 
+              Worked with six full stack developers and one UX designer to create plant management service app. 
+              Worked primarily on front end and pair programmed with back end devs and developer. Created video presentation of project.
             </p>
             <div className="proj1Buttons">
               <div className="proj1Butt">
                 <div>
-                  <Button variant="contained" color="primary" href="https://project-list.now.sh" target="_blank">
+                  <Button variant="contained" color="primary" href="https://plantpurpose.now.sh" target="_blank">
                   View Website
                   </Button>
                 </div>
                 <div>
-                  <Button variant="contained" color="primary" href="https://github.com/cjogles/project-list" target="_blank">
+                  <Button variant="contained" color="primary" href="https://github.com/Plant-Purpose" target="_blank">
                     View Code
+                  </Button>
+                </div>
+                <div>
+                  <Button variant="contained" color="primary" href="https://www.youtube.com/watch?v=s5Ib7fHBSTQlist=PLTrij8iU8hRrvTLbln5H5umHasxrLZPWc" target="_blank">
+                    View Video Presentation
                   </Button>
                 </div>
               </div>
             </div>
           </div>
           <img className="hiddenImg"
-            src="images/todo.jpg" 
-            alt="screenshot of block-club calendar website home page" 
+            src="images/plantScreenShot.jpg" 
+            alt="screenshot of plant purpose website home page" 
             />
         </div>
       </>

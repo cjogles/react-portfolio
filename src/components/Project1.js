762e343f4a749442c4c49dd0bfd557558145e1a3
cjogles <jackson.ogles28@gmail.com>
2020-04-02 13:18:01 -0600

    styling

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -2,47 +2,46 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 const Project1 = () => {
-
+  const butt = {
+    width: '50%',
+    display: 'flex',
+    margin: 'auto',
+    marginBottom: '0.5rem',
+    marginTop: '1rem',
+  }
     return (
       <>
-        <div id="firstProject" className="firstProject1">
-          <a href="https://blockclubcalendar.now.sh" rel="noopener noreferrer" target="_blank">
-            <img 
-            className="imgproj1"
-            src="images/block-club.jpg" 
-            alt="screenshot of block-club calendar website home page" 
-            />
-          </a>
-          <div className="proj1Text">
-            <h2>Block Club Calendar</h2>
-            <p>
-              Worked for a Detroit, MI, non-profit to build
-              a calendar event platform. My work included creating react function components,
-              using JS array methods to dynamically render semantic HTML elements to the DOM,
-              and implementing third party libraries to create UI like a date picker and a login 
-              form. I worked along side four other developers on this project.
-            </p>
-            <div className="proj1Buttons">
-              <div className="proj1Butt">
-                <div>
-                  <Button variant="contained" color="primary" href="https://blockclubcalendar.now.sh" rel="noopener noreferrer" target="_blank">
-                  View Website
-                  </Button>
-                </div>
-                <div>
-                  <Button variant="contained" color="primary" href="https://github.com/Build-Week-Block-Club-Calendar/block-club-calendar-FE/blob/master/README.md" rel="noopener noreferrer" target="_blank">
-                    View README
-                  </Button>
-                </div>
-              </div>
+      <div className="firstProject2">
+      <div className="proj2Text">
+        <h2>Block Club Calendar</h2>
+        <p>
+        Worked for a Detroit, MI, non-profit to build a calendar event platform. My work included creating react function components, using JS array methods to dynamically render semantic HTML elements to the DOM, and implementing third party libraries to create UI like a date picker and a login form. I worked along side four other developers on this project.
+        </p>
+        <div className="proj2Buttons">
+          <div className="proj2Butt">
+            <div>
+              <Button style={butt} variant="contained" color="primary" href="https://blockclubcalendar.now.sh" rel="noopener noreferrer" target="_blank">
+              View Website
+              </Button>
+            </div>
+            <div>
+              <Button style={butt} variant="contained" color="primary" href="https://github.com/Build-Week-Block-Club-Calendar/block-club-calendar-FE/blob/master/README.md" rel="noopener noreferrer" target="_blank">
+                View README
+              </Button>
             </div>
           </div>
-          <img className="hiddenImg imgproj1"
-            src="images/block-club.jpg" 
-            alt="screenshot of block-club calendar website home page" 
-            />
         </div>
-      </>
+      </div>
+      <div className="proj2img">
+        <a href="https://blockclubcalendar.now.sh" rel="noopener noreferrer" target="_blank">
+          <img 
+          src="images/block-club.jpg" 
+          alt="block club website home page" 
+          />
+        </a>
+      </div>
+    </div>
+  </>
     )
 }
 
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -15,57 +15,134 @@ export default class Testimonial extends React.Component {
 
   render() {
     return (
-      <div className="testimony">
-        <p className="testimonyhead">Testimonials 😊 </p>
+
+<div className="project3">
+        <p className="project3head">Testimonials 😊</p>
+        {/* <input
+        type="number"
+        value={this.state.value}
+        onChange={e => this.onChange(parseInt(e.target.value || 0))}
+      /> */}
         <Carousel
-          className="carousel1"
+          className="carousel"
           value={this.state.value}
           onChange={this.onChange}
           slides={[
-            <div className="firstProject1">
+            <div className="firstProject">
               <a
                 href="https://www.linkedin.com/in/amberly-sorensen"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <img
-                  className="mine"
+                  className="testimonial"
                   src="/images/amber.png"
-                  alt="Amber mentioning how great I am"
+                  alt="picture of plant purpose website"
                 />
               </a>
-              <div className="buttons1">
-                <p className="testimonyPerson">Amber Sorensen </p>
-                <p className="testimonyPerson1">Section Lead @ Lambda School (Currently Software Engineer at MMSG)</p>
-                <p className="description">Jackson is an immeasurable asset to my team. He consistently delivers on any task I set him to, and does it with an incredible attitude. His collaborative skills combined with his strong work ethic and influential leadership makes him a valuable resource for his student charges, and a member of my team that helps keeps things running smoothly. Jackson is responsible, reliable, and logical. All those skills combined with his broad knowledge of programming makes him an excellent candidate for a management position. Jackson would flourish and undoubtedly bring a new level of success to any team he is on. 
-                </p>
+              <div className="buttons">
+                <div className='testdiv'>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    href="https://www.linkedin.com/in/amberly-sorensen"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Contact Amber on Linkedin
+                  </Button>
+                </div>
+                <p className='testp'>Jackson is an immeasurable asset to my team. He consistently delivers on any task I set him to, and does it with an incredible attitude. His collaborative skills combined with his strong work ethic and influential leadership makes him a valuable resource for his student charges, and a member of my team that helps keeps things running smoothly. Jackson is responsible, reliable, and logical. All those skills combined with his broad knowledge of programming makes him an excellent candidate for a management position. Jackson would flourish and undoubtedly bring a new level of success to any team he is on. </p>
               </div>
             </div>,
-            <div className="firstProject1">
+            <div className="firstProject">
               <a
                 href="https://www.linkedin.com/in/ira-sanchez"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <img
-                  className="mine"
+                  className="testimonial"
                   src="/images/ira.png"
-                  alt="Ira mentioning how great I am"
+                  alt="picture of plant purpose website"
                 />
               </a>
-              <div className="buttons1">
-                <p className="testimonyPerson">Ira Sanchez </p>
-                <p className="testimonyPerson1">Team Lead @ Lambda School</p>
-                <p className="description"> I’ve had the pleasure of working with Jackson during our 8-week contract as Team Leads, during which we helped manage the academic success of over 150 students. Above all, I was impressed with Jackson’s ability to listen and speak very carefully and with the awareness that his words influence students/his colleagues. Jackson would be a true asset for any positions requiring a front-end engineer who is humble yet personable and comes with my heartfelt recommendation.
-                </p>
+              <div className="buttons">
+                <div className='testdiv'>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    href="https://www.linkedin.com/in/ira-sanchez"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Contact Ira on Linkedin
+                  </Button>
+                </div>
+                <p className='testp'>I’ve had the pleasure of working with Jackson during our 8-week contract as Team Leads, during which we helped manage the academic success of over 150 students. Above all, I was impressed with Jackson’s ability to listen and speak very carefully and with the awareness that his words influence students/his colleagues. Jackson would be a true asset for any positions requiring a front-end engineer who is humble yet personable and comes with my heartfelt recommendation.</p>
               </div>
             </div>
           ]}
           arrows
           clickToChange
           infinite
+          // autoPlay={5000}
+          // animationSpeed={1500}
         />
       </div>
+
+
+      // <div className="testimony">
+      //   <p className="testimonyhead">Testimonials 😊 </p>
+      //   <Carousel
+      //     className="carousel1"
+      //     value={this.state.value}
+      //     onChange={this.onChange}
+      //     slides={[
+      //       <div className="firstProject1">
+      //         <a
+      //           href="https://www.linkedin.com/in/amberly-sorensen"
+      //           rel="noopener noreferrer"
+      //           target="_blank"
+      //         >
+      //           <img
+      //             className="mine"
+      //             src="/images/amber.png"
+      //             alt="Amber mentioning how great I am"
+      //           />
+      //         </a>
+      //         <div className="buttons1">
+      //           <p className="testimonyPerson">Amber Sorensen </p>
+      //           <p className="testimonyPerson1">Section Lead @ Lambda School (Currently Software Engineer at MMSG)</p>
+      //           <p className="description">Jackson is an immeasurable asset to my team. He consistently delivers on any task I set him to, and does it with an incredible attitude. His collaborative skills combined with his strong work ethic and influential leadership makes him a valuable resource for his student charges, and a member of my team that helps keeps things running smoothly. Jackson is responsible, reliable, and logical. All those skills combined with his broad knowledge of programming makes him an excellent candidate for a management position. Jackson would flourish and undoubtedly bring a new level of success to any team he is on. 
+      //           </p>
+      //         </div>
+      //       </div>,
+      //       <div className="firstProject1">
+      //         <a
+      //           href="https://www.linkedin.com/in/ira-sanchez"
+      //           rel="noopener noreferrer"
+      //           target="_blank"
+      //         >
+      //           <img
+      //             className="mine"
+      //             src="/images/ira.png"
+      //             alt="Ira mentioning how great I am"
+      //           />
+      //         </a>
+      //         <div className="buttons1">
+      //           <p className="testimonyPerson">Ira Sanchez </p>
+      //           <p className="testimonyPerson1">Team Lead @ Lambda School</p>
+      //           <p className="description"> I’ve had the pleasure of working with Jackson during our 8-week contract as Team Leads, during which we helped manage the academic success of over 150 students. Above all, I was impressed with Jackson’s ability to listen and speak very carefully and with the awareness that his words influence students/his colleagues. Jackson would be a true asset for any positions requiring a front-end engineer who is humble yet personable and comes with my heartfelt recommendation.
+      //           </p>
+      //         </div>
+      //       </div>
+      //     ]}
+      //     arrows
+      //     clickToChange
+      //     infinite
+      //   />
+      // </div>
     );
   }
 }

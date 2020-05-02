cf075f1b2432173b5c5ff9c2d9726b09ed82e4ba
cjogles <jackson.ogles28@gmail.com>
2020-05-01 23:07:17 -0600

    edited spelling error

--- src/components/Skillz.js
+++ src/components/Skillz.js
@@ -9,7 +9,7 @@ const data = [
  ["JavaScript", "I understand OOP paradigms, Closures, Higher Order Functions, Variable Scope & Hoisting, TDD, asyncronous programming with promises, and functional programming paradigms"],
  ["React", "I understand how to utilize Function Components with Hooks, Class Components with Life Cycle Methods, Higher Order Components (i.e. with Formik), and how to use Context API and drill props. I also know how to use React Router."],
  ["Redux", "I know how to manage an applications global state through the redux work flow i.e. creating Actions, Action Creators, Reducers, Store, the Connect Function, MapStateToProps, and asynchronously requesting and changing state with axios."],
- ["Back End", "I know how to use NodeJS to create a server and back end API using Express, Knex, Sqlite3, PostgresQL, and SQL. I know how to implement middleware to provide an authentication layer on the Server API. I am continually learning to handle more copmlex database queries."],
+ ["Back End", "I know how to use NodeJS to create a server and back end API using Express, Knex, Sqlite3, PostgresQL, and SQL. I know how to implement middleware to provide an authentication layer on the Server API. I am continually learning to handle more complex database queries."],
  ["Version Control", "I try to create detailed Commit History for group projects, logically plan out software architecutre via sound process models. I know GIT, Github, and how to manage merge conflicts."],
  ["People Skills", "I have a Bachelors of Science in Psychology, mentoring experience in web development as a team lead at Lambda School, customer service experience, as well as mental health work and child welfare casework experience"],
 ];
